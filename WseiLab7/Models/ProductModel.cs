33ee858e2070e6728ad316460d0f4728144a139d
Maksymilian Jesionka <maksymilian.jesionka@microsoft.wsei.edu.pl>
2021-10-09 16:27:27 +0200

    ce

---
+++ WseiLab7/Models/ProductModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WseiLab7.Models
+{
+    public class ProductModel
+    {
+        public string Name { get; set; }
+    
+        public string Description { get; set; }
+
+        public bool IsVisible { get; set; }
+
+    }
+}
